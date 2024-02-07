fe79df3aaa968a9255c6636f188b88ae0c69c682
Garaisourav12 <garaisourav12@gmail.com>
2024-02-07 13:24:16 +0530

    media query

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         prevArrow: <IoIosArrowDropleft className='slick-arrow' />,
         nextArrow: <IoIosArrowDropright className='slick-arrow' />

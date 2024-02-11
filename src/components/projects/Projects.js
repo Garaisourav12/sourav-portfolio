e98214d12387499628f4b602be5537ab84f5aac4
Garaisourav12 <garaisourav12@gmail.com>
2024-02-11 13:05:32 +0530

    project card

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,23 +6,25 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import yt from '../../assets/yt.png';
+import fp from '../../assets/flickipedia.png';
+import imb from '../../assets/imagebazzer.png';
 import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
 
 function Projects() {
     const projects = [
         {
-            title: 'YouTube Clone',
-            description: 'YouTube Clone with JavaScript, utilizing APIs to replicate key functionalities of the popular video-sharing platform, including video playback, search.',
-            image: yt,
-            repo_link: 'https://github.com/Garaisourav12/youtube-clone',
-            live_link: 'https://Garaisourav12.github.io/youtube-clone'
+            title: 'Flickipedia',
+            description: 'A comprehensive movie & tv show information app using React, offering users detailed insights, ratings, trailer and search capabilities for an enriched cinematic experience.',
+            image: fp,
+            repo_link: 'https://github.com/Garaisourav12/Flickipedia',
+            live_link: 'https://flickipedia.vercel.app/'
         },
         {
-            title: 'YouTube Clone',
-            description: 'YouTube Clone with JavaScript, utilizing APIs to replicate key functionalities of the popular video-sharing platform, including video playback, search.',
-            image: yt,
-            repo_link: 'https://github.com/Garaisourav12/youtube-clone',
-            live_link: 'https://Garaisourav12.github.io/youtube-clone'
+            title: 'Image Bazzer',
+            description: 'The React ImageBazzer project leverages the Unsplash API to dynamically fetch and display a wide array of high-quality images. Users can search for specific images, explore curated collections.',
+            image: imb,
+            repo_link: 'https://github.com/Garaisourav12/Image-Bazzer',
+            live_link: 'https://image-bazzer.vercel.app/'
         },
         {
             title: 'YouTube Clone',
